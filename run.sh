123c3ba36e1186e1c6d99e1dcd83b42985193a6b
orlandomotapires <orlandomota2002@gmail.com>
2024-03-23 18:57:43 -0300

    [HOTFIX] Run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile Main.java
-javac Main.java
+javac main.java
 
 # Clear time_analyses.txt file
 > ./txt_files/time_analyses.txt
@@ -9,7 +9,7 @@ javac Main.java
 # Loop through thread_num and pod values
 for thread_num in 1 5 10; do
     for pod in $(seq 1 100); do
-        java Main $thread_num $pod
+        java main $thread_num $pod
     done
 done
 
