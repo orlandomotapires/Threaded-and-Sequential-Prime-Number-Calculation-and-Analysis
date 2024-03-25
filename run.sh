819520d8765f22995969d48b744c076d35eb4116
wesleylelo <65774990+wesleylelo@users.noreply.github.com>
2024-03-24 23:04:53 -0300

    Update run.sh
    Commit change with Sleep and Echo

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Compile Main.java
+echo("Starting the compilation")
+sleep(1)
 javac main.java
-
+echo("Compilation Success")
+sleep(1)
 # Clear time_analyses.txt file
 > ./txt_files/time_analyses.txt
-
 # Loop through thread_num and pod values
 for thread_num in 1 5 10; do
     for pod in $(seq 1 100); do
